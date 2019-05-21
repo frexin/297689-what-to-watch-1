386da41945d96fbafe5083a7ff470dbd3defdaa8
Senkevich Kirill <sks89@mail.ru>
2019-05-22 02:56:51 +0300

    Добавление тестов для GenresList и reducer

--- src/components/main-page.test.js
+++ src/components/main-page.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import MainPage from './main-page.jsx';
+import {MainPage} from './main-page.jsx';
 
 import {movies} from "../mocks/films";
 
 it(`Main components loads correctly`, () => {
-  const tree = renderer.create(<MainPage movies={movies} />).toJSON();
+  const tree = renderer.create(<MainPage currentGenre={`All genres`} onGenreSelect={jest.fn()} movies={movies} />).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
